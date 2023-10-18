5658fd3937711fc9d3ecf5555f9d7f43692a351c
srilali26 <124224140+srilali26@users.noreply.github.com>
2023-10-18 20:03:55 +0530

    (6).py

--- (6).py
+++ (6).py
@@ -1,4 +1,10 @@
-IRIS = load_IRIS()
+from sklearn.datasets import load_iris
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+
+iris = load_iris()
 
 
 X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3, random_state=0)
